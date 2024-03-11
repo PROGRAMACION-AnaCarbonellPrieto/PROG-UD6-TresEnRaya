282d29c0fd7bcb9e241e5cccaf9ca10c1c36e523
Ana Carbonell Prieto <anacarpri@alu.edu.gva.es>
2024-03-11 18:28:23 +0100

    Estructura inicial

---
+++ src/main/java/prog/ud6/tresenraya/TresEnRaya.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package prog.ud6.tresenraya;
+
+/**
+ *
+ * @author Ana Carbonell Prieto
+ */
+public class TresEnRaya {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
